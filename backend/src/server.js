95053aa808d1b21825144980546f8ac6b82819e4
ribhupramanik <ribhu02@gmail.com>
2026-02-09 21:41:54 +0530

    Made changes to make app deployable

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
 import express from "express"
-import dotenv from "dotenv"
+import { ENV } from "./lib/env.js"
+import path from "path"
 
 const app = express()
-dotenv.config()
+
+const __dirname = path.resolve()
 
 app.get("/", (req, res) => {
   res.status(200).json({message: "success"})
 })
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
+if(ENV.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+  app.get("/{*any}", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
+
+
+app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`)) 
