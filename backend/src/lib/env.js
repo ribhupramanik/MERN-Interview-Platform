ebd94b28b74ae132bd0b27dfec4052a1e80cc8c9
ribhupramanik <ribhu02@gmail.com>
2026-02-10 21:29:44 +0530

    Changed name of env variable to ensure smooth connection to inngest

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,8 +5,8 @@ export const ENV= {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
   NODE_ENV: process.env.NODE_ENV,
-  INGEST_EVENT_KEY: process.env.INGEST_EVENT_KEY,
-  INGEST_SIGNING_KEY: process.env.INGEST_SIGNING_KEY,
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
   STREAM_API_KEY: process.env.STREAM_API_KEY,
   STREAM_SECRET_KEY: process.env.STREAM_SECRET_KEY,
   CLIENT_URL: process.env.CLIENT_URL
