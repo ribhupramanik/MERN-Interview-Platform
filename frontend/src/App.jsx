e2d96d5e638936cb437a07b8dbb5e316d87c0532
ribhupramanik <ribhu02@gmail.com>
2026-02-13 21:35:13 +0530

    Installed tailwind css and daisyui and set it up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
 import {SignInButton, SignOutButton, SignedIn, SignedOut, UserButton} from "@clerk/clerk-react"
+import {Routes, Route} from "react-router"
 
 function App() {
   return (
-    <>
-      <h1>Wlcome to app</h1>
+    <Routes>
+      <h1>Welcome to app</h1>
+
+     
       <SignedOut>
         <SignInButton mode='modal'>
           <button>Login</button>
@@ -16,7 +18,7 @@ function App() {
       </SignedIn>
       
       <UserButton/>
-    </>
+    </Routes>
   )
 }
 
