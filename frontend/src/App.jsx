4c9f6ec8be1a5a975f20cfb420ec910f0480b65b
ribhupramanik <ribhu02@gmail.com>
2026-02-13 21:53:27 +0530

    React hot toast setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {SignInButton, SignOutButton, SignedIn, SignedOut, UserButton, useUser} from "@clerk/clerk-react"
 import {Routes, Route, Navigate} from "react-router"
+import {Toaster} from "react-hot-toast"
 import HomePage from "./pages/HomePage"
 import ProblemsPage from "./pages/ProblemsPage"
 
@@ -7,10 +8,13 @@ function App() {
 
   const {isSignedIn}=  useUser()
   return (
+    <>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
     </Routes>
+    <Toaster/>
+    </>
   )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
 import React from "react";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
   return (
     <div>
-      <button className="btn btn-secondary">Click me</button>
+      <button className="btn btn-secondary" onClick={() => toast.success("Button Clicked")}>Click me</button>
       <SignedOut>
         <SignInButton mode="modal">
           <button>Login</button>
