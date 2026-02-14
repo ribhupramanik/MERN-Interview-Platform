23edc1876ac0506f01d0979c8be122681172b146
ribhupramanik <ribhu02@gmail.com>
2026-02-14 23:19:02 +0530

    Setup tanstack query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,12 +15,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
 }
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client = {queryClient}>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App />
       </ClerkProvider>
+    </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
