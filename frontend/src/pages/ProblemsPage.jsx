57c8cc70f2ff3b3ad171de1b37eddc9760e47fd3
ribhupramanik <ribhu02@gmail.com>
2026-02-15 22:25:00 +0530

    Created the navbar component

--- frontend/src/pages/ProblemsPage.jsx
+++ frontend/src/pages/ProblemsPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const ProblemsPage = () => {
   return (
-    <div>ProblemsPage</div>
+    <div className='min-h-screen bg-base-200'>
+      <Navbar/>
+
+    </div>
   )
 }
 
