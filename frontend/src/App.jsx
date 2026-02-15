11ec11eefee524a124d0830c7a308b89f5997b15
ribhupramanik <ribhu02@gmail.com>
2026-02-15 21:40:31 +0530

    Created the dashboard route and check to ensure logged in users are redirected to dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,45 @@
-import {SignInButton, SignOutButton, SignedIn, SignedOut, UserButton, useAuth} from "@clerk/clerk-react"
-import {Routes, Route, Navigate} from "react-router"
-import {Toaster} from "react-hot-toast"
-import HomePage from "./pages/HomePage"
-import ProblemsPage from "./pages/ProblemsPage"
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useAuth,
+} from "@clerk/clerk-react";
+import { Routes, Route, Navigate } from "react-router";
+import { Toaster } from "react-hot-toast";
+import HomePage from "./pages/HomePage";
+import ProblemsPage from "./pages/ProblemsPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
-
-  const {isSignedIn, isLoaded}=  useAuth()
-    if (!isLoaded) {
-      return (
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) {
+    return (
       <div className="min-h-screen flex items-center justify-center bg-base-100">
         <span className="loading loading-spinner loading-xl text-primary"></span>
       </div>
-    )
-    }
+    );
+  }
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
-    </Routes>
-    <Toaster/>
+      <Routes>
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+      <Toaster />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
