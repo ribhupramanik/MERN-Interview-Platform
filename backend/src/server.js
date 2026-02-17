0a386c7762c3d839a84ec6fb65483ce33dd11573
ribhupramanik <ribhu02@gmail.com>
2026-02-17 22:07:35 +0530

    Rearranged the routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,10 +19,10 @@ app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
 app.use(clerkMiddleware())
 
+app.use("/api/execute", executeRoute);
 app.use("/api/inngest", serve({client:inngest, functions}))
 app.use("/api/chat", chatRoutes)
 app.use('/api/sessions', sessionRoutes)
-app.use("/api/execute", executeRoute);
 
 app.get("/books", (req, res) => {
   res.status(200).json({message: "success"})
