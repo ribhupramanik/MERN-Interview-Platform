6b4cd86f63f6e632f7c23dbb6fba6127760757a0
ribhupramanik <ribhu02@gmail.com>
2026-02-18 20:26:10 +0530

    Created mutation to join and end session

--- frontend/src/hooks/useSessions.js
+++ frontend/src/hooks/useSessions.js
@@ -33,10 +33,30 @@ export const useMyRecentSessions = () => {
 export const useSessionById = (id) => {
   const result = useQuery({
     queryKey: ["session",id],
-    queryFn: sessionApi.getSessionById(id),
+    queryFn: () => sessionApi.getSessionById(id),
     enabled: !!id,
     refetchInterval: 5000,
   })
 
+  return result
+}
+
+export const useJoinSession = (id) => {
+  const result = useMutation({
+    mutationKey: ["joinSession"],
+    mutationFn: () => sessionApi.joinSession(id),
+    onSuccess: () => toast.success("Joined Session Successfully"),
+    onError: (error) => toast.error(error.response?.data?.message || "Failed to join session"),
+  })
+  return result
+}
+
+export const useEndSession = (id) => {
+  const result = useMutation({
+    mutationKey: ["endSession"],
+    mutationFn: () => sessionApi.endSession(id),
+    onSuccess: () => toast.success("Session Ennded Successfully"),
+    onError: (error) => toast.error(error.response?.data?.message || "Failed to end session"),
+  })
   return result
 }
