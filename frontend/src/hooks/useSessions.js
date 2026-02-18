73fe2b692b02fc8d6d4332c6d8ec32f1e5eef21d
ribhupramanik <ribhu02@gmail.com>
2026-02-18 20:19:08 +0530

    Create mutation to create new Session and query to fetch recent sessions

--- frontend/src/hooks/useSessions.js
+++ frontend/src/hooks/useSessions.js
@@ -2,12 +2,29 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast"
 import { sessionApi } from "../api/sessions";
 
-
+export const useCreateSession = () => {
+  const result = useMutation({
+    mutationKey: ["createSession"],
+    mutationFn: sessionApi.createSession,
+    onSuccess: () => toast.success("Session Created Successfully"),
+    onError: (error) => toast.error(error.response?.data?.message || "failed to create room") 
+  })
+  return result
+}
 
 export const useActiveSessions = () => {
   const result = useQuery({
     queryKey: ["activeSessions"],
     queryFn: sessionApi.getActiveSessions
+  })
+
+  return result
+}
+
+export const useMyRecentSession = () => {
+  const result = useQuery({
+    queryKey: ["myRecentSessions"],
+    queryFn: sessionApi.getMyRecentSessions,
   })
 
   return result
