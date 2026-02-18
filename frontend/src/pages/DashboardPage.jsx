f5c609edba863fad6742dfd74669c85239cf5a8c
ribhupramanik <ribhu02@gmail.com>
2026-02-18 22:11:47 +0530

    Created handleRoom creation function which allows a user to create a room

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { useUser } from "@clerk/clerk-react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import {
+  useActiveSessions,
+  useCreateSession,
+  useMyRecentSessions,
+} from "../hooks/useSessions";
+import Navbar from "../components/Navbar";
+import WelcomeSection from "../components/WelcomeSection";
+import StatCards from "../components/StatCards";
+import ActiveSessions from "../components/ActiveSessions";
+import RecentSessions from "../components/RecentSessions";
+import CreateSessionModel from "../components/CreateSessionModel";
 
 const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
-}
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const [showCreateModel, setShowCreateModel] = useState(false);
+  const [roomConfig, setRoomConfig] = useState({ problem: "", difficulty: "" });
 
-export default DashboardPage
+  const createSessionMutation = useCreateSession();
+  const { data: activeSessionsData, isLoading: loadingActiveSessions } =
+    useActiveSessions();
+  const { data: recentSessionsData, isLoading: loadingRecentSessions } =
+    useMyRecentSessions();
+
+  const handleCreateRoom = () => {
+    if (!roomConfig.problem || !roomConfig.difficulty) return;
+    createSessionMutation.mutate(
+      {
+        problem: roomConfig.problem,
+        difficulty: roomConfig.difficulty.toLowerCase(),
+      },
+      {
+        onSuccess: (data) => {
+          setShowCreateModel(false);
+          navigate(`/session${data.session._id}`);
+        },
+      },
+    );
+  };
+
+  const activeSessions = activeSessionsData?.sessions || [];
+  const recentSessions = recentSessionsData?.sessions || [];
+
+  return (
+    <>
+      <div className="min-h-screen bg-base-300">
+        <Navbar />
+        <WelcomeSection
+          onCreateSession={() => {
+            setShowCreateModel(true);
+          }}
+        />
+        {/* Grid Layout */}
+
+        <div className="conatainer mx-auto px-6 pb-16">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <StatCards />
+            <ActiveSessions />
+          </div>
+
+          <RecentSessions />
+        </div>
+      </div>
+      <CreateSessionModel
+        isOpen={showCreateModel}
+        onClose={() => setShowCreateModel(false)}
+        roomConfig={roomConfig}
+        setRoomConfig={setRoomConfig}
+        onCreateRoom={handleCreateRoom}
+        isCreating={createSessionMutation.isPending}
+      />
+    </>
+  );
+};
+
+export default DashboardPage;
